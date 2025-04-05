25f05df3c23374aa34b486599d9e46c08fd8c24f
ApplBoy <saulojosesilva030@gmail.com>
2025-04-05 04:53:45 -0300

    Fixes for bootstrap
    Signed-off-by: ApplBoy <saulojosesilva030@gmail.com>

--- bootstrap/yum.sh
+++ bootstrap/yum.sh
@@ -14,7 +14,8 @@ gh auth status -h github.com >/dev/null 2>&1&&{ printf "${B}Already authenticate
 d=$(basename "$r");[ -d "$d" ]&&{ printf "${B}Re-clone? [y/N] ${N}";read y;[[ $y =~ ^[Yy]$ ]]&&rm -rf "$d"&&gh repo clone "$r"||cd "$d"&&git pull&&cd -; }||gh repo clone "$r"
 type -p docker||{ sudo yum -y install dnf-plugins-core;sudo yum config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo;sudo yum -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;sudo systemctl enable --now docker;sudo docker run hello-world;}
 sudo usermod -aG docker "$USER"
-(! docker info|grep -q Username||{ printf "${B}Already logged in. Re-login? [y/N] ${N}";read d;[[ $d =~ ^[Yy]$ ]];})&&docker logout ghcr.io 2>/dev/null;([[ $tokendocker == 1 ]]&&echo "$t"|docker login ghcr.io -u "$u" --password-stdin||docker login ghcr.io -u "$u")
+grep -q '"ghcr.io"' ~/.docker/config.json 2>/dev/null&&{ printf "${B}Already logged in. Re-login? [y/N] ${N}";read d;[[ $d =~ ^[Yy]$ ]]&&docker logout ghcr.io; }||true
+([[ $tokendocker == 1 ]]&&echo "$t"|docker login ghcr.io -u "$u" --password-stdin||docker login ghcr.io -u "$u")
 [ -d ~/.local/src/fzf ]||{ mkdir -p ~/.local/src;git clone --depth 1 https://github.com/junegunn/fzf.git ~/.local/src/fzf;~/.local/src/fzf/install --key-bindings --completion --no-update-rc;}
 type -p kubectl||{ printf "[kubernetes]\nname=Kubernetes\nbaseurl=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/\nenabled=1\ngpgcheck=1\ngpgkey=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/repodata/repomd.xml.key"|sudo tee /etc/yum.repos.d/kubernetes.repo;sudo yum -y install kubectl;}
 sudo yum -y install epel-release&&grep -q "Oracle Linux" /etc/os-release&&sudo yum -y install oracle-epel-release-el8&&sudo yum config-manager --enable ol8_developer_EPEL;sudo yum -y update
@@ -23,3 +24,5 @@ IFS=',' read -ra db_arr<<<"$dbs";for db in "${db_arr[@]}";do
 [[ $db == mariadb-10.4 ]]&&{ sudo yum -y install wget;wget -q https://downloads.mariadb.com/MariaDB/mariadb_repo_setup;chmod +x mariadb_repo_setup;sudo ./mariadb_repo_setup;rm -f mariadb_repo_setup;sudo yum -y install MariaDB-server;sudo systemctl start mariadb;sudo mariadb-secure-installation; };
 [[ $db == postgres-17 ]]&&{ sudo yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm;sudo yum -qy module disable postgresql;sudo yum -y install postgresql17-server;sudo /usr/pgsql-17/bin/postgresql-17-setup initdb;sudo systemctl enable postgresql-17;sudo systemctl start postgresql-17; };done
 [[ $node == 1 ]]&&curl -sL https://rpm.nodesource.com/setup_18.x|sudo bash -&&sudo yum -y install nodejs
+
+cd "$d"&&{ [[ -d deploy ]]&&git submodule update --init --recursive||git clone git@github.com:ApplBoy/deploy.git deploy; }&&cd deploy&&exec bash deploy.sh
